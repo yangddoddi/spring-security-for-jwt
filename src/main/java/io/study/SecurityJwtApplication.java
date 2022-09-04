ff3587488d65f535f1de3b111be25c5245f6a93a
yangddoddi <pud1251@gmail.com>
2022-09-04 20:36:48 +0900

    first commit

---
+++ src/main/java/io/study/SecurityJwtApplication.java
@@ -0,0 +1,23 @@
+package io.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+*
+* 기본 구성
+* BASE64(Header)
+* BASE64(Payload)
+* BASE64(HS256Secure(LowSig))
+* 
+* */
+
+
+@SpringBootApplication
+public class SecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityJwtApplication.class, args);
+    }
+
+}
